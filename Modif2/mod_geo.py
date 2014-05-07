189423f7cb08e36f4fd925a16867644b76b46263
Jesus Rodriguez Falcón <alu0100099010@ull.edu.es>
2014-05-07 08:25:56 +0100

    Update mod_geo.py

--- Modif2/mod_geo.py
+++ Modif2/mod_geo.py
@@ -1,3 +1,19 @@
+###################################################################################
+# mod_geo.py
+###################################################################################
+#
+# AUTORES: María Baeza López
+#         Juan Jesús Dóniz Labrador
+#         Jesús Manuel Rodríguez Falcón
+#
+# FECHA:
+#
+# DESCRIPCION: Este módulo contiene dos funciones:
+#             1. calcular_geo (n,p)---->calcula la probabilidad geométrica y la devuelve a sol_geo.py
+#             2. calcular_geo1 (n,p)--->Calcula la probabilidad geométrica desde 1 hasta n y las va almacenando 
+#                en sumaprobabilidad
+###################################################################################
+
 #!/usr/bin/python
 #!encoding: UTF-8
 import sys
